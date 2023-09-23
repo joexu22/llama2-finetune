bd2a25be716fd74137f6f6e16dc1644a5f83e789
JX <joegxu22@gmail.com>
2023-09-23 04:45:54 -0400

    OpenAI don't charge me too much...

--- data_processing/openai_datagen.py
+++ data_processing/openai_datagen.py
@@ -2,21 +2,30 @@ import openai
 import os
 from dotenv import load_dotenv
 
+# Load API key from environment variable
 load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
+
+# Initialize OpenAI API client
 openai.api_key = api_key
 
-questions = [
-    "What are MSE and RMSE?",
-    "Explain DBSCAN algorithm?",
-    "What are dummy variables?"
-]
+# Read questions from a text file, one question per line, and filter out blank lines
+with open("questions.txt", "r") as f:
+    questions = [line.strip() for line in f.readlines() if line.strip()]
 
-for question in questions:
-    prompt = f"Explain {question} in a simple and relatable way."
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=150
-    )
-    print(f"{question}\n{response.choices[0].text.strip()}\n")
+# Open the output file for writing answers
+with open("answers.txt", "w") as output_file:
+    # Iterate over each question and get the answer
+    for question in questions:
+        prompt = f"You are an expert and wise llama shepard who is secretly a machine learning expert who has succeeded in building fully autonomous AGIs before retiring. You are asked to explain the following question {question} to a 5 year old kid. Please provide concrete and relatable examples that a 5 year old can reproduce. There should be a steady reference to llamas examples. You're prone to let leak your deep mathematical and philosophical insight. You capture the essence of the question with clarity and elucidate the audience."
+        response = openai.Completion.create(
+            engine="gpt-3.5-turbo-instruct",
+            prompt=prompt,
+            max_tokens=1000
+        )
+        
+        # Remove line breaks from the answer
+        cleaned_text = response.choices[0].text.strip().replace('\n', ' ')
+        
+        # Write the question and the cleaned answer to the output file
+        output_file.write(f"{question}\n{cleaned_text}\n")
